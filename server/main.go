2c3726f59d43cb25655baa4ebff502886e517067
Rita <up202004155@up.pt>
2023-11-24 10:58:43 +0000

    db

--- server/main.go
+++ server/main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
+	"sdle/m/v2/api"
+	"sdle/m/v2/database"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	fmt.Println("Hello from server")
+	const filename = "server.db"
+	db, err := sql.Open("sqlite3", filename)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sqliteRepository := database.NewSQLiteRepository(db)
+	createError := sqliteRepository.CreateTables()
+	if createError != nil {
+		fmt.Println(createError.Error())
+		return
+	}
+	
+	api.SetDB(sqliteRepository)
+
+	if apiDB := api.GetDB(); apiDB == nil {
+		fmt.Print("Database not initialized in API")
+		return
+	}
+
 }
