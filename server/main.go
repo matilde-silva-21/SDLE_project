41c779e536e4332e44b5f8c109c1db2796b6c4c4
Matilde Silva <matilde.m.silva.21@gmail.com>
2023-11-27 17:52:17 +0000

    clean up dead files

--- server/main.go
+++ server/main.go
@@ -2,46 +2,9 @@ package main
 
 import (
 	"fmt"
-	"sdle/server/CRDT/addWinSet"
 	"sdle/server/CRDT/lexCounter"
 	shoppingList "sdle/server/CRDT/shoppingList"
 )
-
-func SetExample(){
-
-	CRDT1 := addWinSet.CreateSet()
-	CRDT2 := addWinSet.CreateSet()
-
-
-	fmt.Println(CRDT1)
-
-	addWinSet.Add("apple", &CRDT1)
-	addWinSet.Add("pear", &CRDT1)
-
-	addWinSet.Add("cheese", &CRDT2)
-	addWinSet.Add("milk", &CRDT2)
-
-
-	fmt.Println("\nOp1", CRDT1)
-	fmt.Println(addWinSet.Contains("apple", CRDT1))
-	
-	addWinSet.Remove("apple", &CRDT1)
-	addWinSet.Remove("basil", &CRDT2)
-
-	
-	fmt.Println("\nOp2", CRDT1)
-	fmt.Println(addWinSet.Contains("apple", CRDT1))
-
-	set3 := addWinSet.MergeSets(CRDT1, CRDT2)
-
-	fmt.Println("\nOp3", set3)
-
-	addWinSet.Remove("milk", &set3)
-
-	fmt.Println("\nOp4", set3)
-	fmt.Println(addWinSet.Contains("milk", set3))
-
-}
 
 func LexExample(){
 	x := lexCounter.Create[string, int]("a")
