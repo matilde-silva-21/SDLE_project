d4e12593aad307b6d873bf78c4bd637788cad4e7
Matilde Silva <matilde.m.silva.21@gmail.com>
2023-12-06 21:57:02 +0000

    clean up and refactor code

--- server/serverCommunicator/serverCommunicator.go
+++ server/serverCommunicator/serverCommunicator.go
@@ -10,6 +10,10 @@ import (
 	//"sdle/server/utils/CRDT/shoppingList"
 )
 
+type ChanPair struct {
+	channel  chan []byte
+	ready chan struct{}
+}
 
 // Get preferred outbound ip of this machine
 func GetOutboundIP() string {
@@ -95,7 +99,7 @@ func listenToOrchestrator(conn *net.TCPConn) error {
 
 }
 
-func ExecuteQuorum(conn *net.TCPConn, connChannel chan []byte, payload messageStruct.MessageStruct) error{
+func ExecuteQuorum(conn *net.TCPConn, chanPair ChanPair, payload messageStruct.MessageStruct) error{
 
 	payload.Action = messageStruct.Read;
 	fmt.Println("\n\n", payload, "\n")
@@ -106,31 +110,79 @@ func ExecuteQuorum(conn *net.TCPConn, connChannel chan []byte, payload messageSt
 
 	buffer := make([]byte, 1024)
 
-	// Ler o CRDT de lá
+	// Ler o CRDT que vem do outro server
 	n, err := conn.Read(buffer)
-
-	fmt.Println("i read something and i am happy")
 
 	if err != nil { return err } // TODO se houver erro A QUALQUER MOMENTO DA CONEXÃO
 
 	// Meter o CRDT de lá no chan
-	connChannel <- (buffer[:n])
+	chanPair.channel <- (buffer[:n])
 
-	// Add a short delay so the other thread can read the channel and send the message
-	time.Sleep(100 * time.Millisecond)
+	// Wait for the signal that channel was read by the calling thread
+	<- chanPair.ready
 
 	// Mandar o novo CRDT de volta para lá
-	messageToSend := <- connChannel
+	messageToSend := <- chanPair.channel
 
 	conn.Write(messageToSend)
+
+	chanPair.ready <- struct{}{}
+
+	conn.Close()
 
 	return nil
 }
 
 
+func PollQuorumChannels(channelsMap *map[string](ChanPair), listResponses *map[string]([]byte), payload messageStruct.MessageStruct){
+
+	// Set a timeout for the quorum
+	timeout := time.After(10 * time.Second)
+
+	// Poll channels
+	for {
+		fmt.Println("loop 1", *channelsMap)
+		for key, ch := range *channelsMap {
+			fmt.Println("loop 2")
+
+			select {
+				case data := <-(ch.channel):
+					log.Printf("My friend %s answered me!", key)
+					ch.ready <- struct{}{}
+					(*listResponses)[key] = data
+				
+				case <-timeout:
+					// Break out of the loop when the timeout is reached
+					log.Print("Timeout reached. Aborting Quorum.")
+					// TODO mandar mensagem de erro de volta ao Orch
+					return
+			}
+		}
+		if(len(*channelsMap) == len(*listResponses)) {
+			// TODO read and merge all CRDTs and send back responses (nao esquecer de mandar o novo CRDT ao Orch)
+			
+			fmt.Println("\n\neveryone came to the party :)\nnow bye-bye!")
+
+			newCRDTMessage := payload.ToJSON() // placeholder
+			for _, ch := range *channelsMap {
+				ch.channel <- newCRDTMessage
+			}
+
+			// End the program only when all threads have already been read
+			for _, ch := range *channelsMap {
+				<- ch.ready
+			}
+
+			return
+		}
+	}
+
+}
+
+
 func StartQuorumConnection(IPs []string, payload messageStruct.MessageStruct){
 
-	channelsMap := make(map[string](chan []byte))
+	channelsMap := make(map[string](ChanPair))
 	listResponses := make(map[string]([]byte))
 
 	minNumConn := (len(IPs) - len(IPs)%2) + 1
@@ -155,7 +207,7 @@ func StartQuorumConnection(IPs []string, payload messageStruct.MessageStruct){
 		connChannel := make(chan []byte, 3) // Create Channel for the TCP connection know which messages should be sent
 		defer close(connChannel)
 
-		channelsMap[ip] = connChannel
+		channelsMap[ip] = ChanPair{channel: connChannel, ready: make(chan struct{})}
 
 		connections = append(connections, conn)
 		activeConn += 1
@@ -163,7 +215,7 @@ func StartQuorumConnection(IPs []string, payload messageStruct.MessageStruct){
 
 
 
-		go ExecuteQuorum(conn, connChannel, payload)
+		go ExecuteQuorum(conn, channelsMap[ip], payload)
 
 		if (minNumConn <= activeConn) { break }
 
@@ -171,43 +223,9 @@ func StartQuorumConnection(IPs []string, payload messageStruct.MessageStruct){
 
     // TODO abortar o quorum if connection not successful
 
+	PollQuorumChannels(&channelsMap, &listResponses, payload)
 
-	// Set a timeout for the quorum
-	timeout := time.After(10 * time.Second)
-
-	// Poll channels
-	for {
-		fmt.Println("loop 1", channelsMap)
-		for key, ch := range channelsMap {
-			fmt.Println("loop 2")
-
-			select {
-				case data := <-ch:
-					log.Printf("My friend %s answered me!", key)
-					listResponses[key] = data
-				
-				case <-timeout:
-					// Break out of the loop when the timeout is reached
-					log.Print("Timeout reached. Aborting Quorum.")
-					// TODO mandar mensagem de erro de volta ao Orch
-					return
-			}
-		}
-		if(len(channelsMap) == len(listResponses)) {
-			// TODO read and merge all CRDTs and send back responses (nao esquecer de mandar o novo CRDT ao Orch)
-			
-			fmt.Println("\n\neveryone was here :)\nnow bye-bye!")
-
-			newCRDTMessage := payload.ToJSON() // placeholder
-			for _, ch := range channelsMap {
-				ch <- newCRDTMessage
-			}
-			// Add a short delay so the other threads can read the channel and send the message
-			time.Sleep(100 * time.Millisecond)
-			return
-		}
-	}
-
+	return
 }
 
 func listenToConnection(conn *net.TCPConn) error {
